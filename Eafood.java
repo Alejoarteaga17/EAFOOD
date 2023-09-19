c9425359f69128f6175dad0a5eed4143532ce9ef
Alejoarteaga17 <120742659+Alejoarteaga17@users.noreply.github.com>
2023-09-19 18:55:07 -0500

    Create Eafood.java

---
+++ Eafood.java
@@ -0,0 +1,14 @@
+
+package eafoodMain;
+
+import eafoodPpal.Principal;
+
+public class EAFOOD {
+
+    public static void main(String[] args) {
+        Principal princi = new Principal();
+        princi.setVisible(true);
+        princi.setLocationRelativeTo(null);
+    
+    }
+  }
